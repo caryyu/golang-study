63e5855f38f177237ddf062a549ab800e196edbb
liang.yu <liang.yu@nakedhub.com>
2017-12-22 11:35:11 +0800

    commit

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
+
 import "fmt"
 
+// Integer ...
 type Integer int32
 
-func checkIf(i Integer) bool  {
-  return i > 10;
+func checkIf(i Integer) bool {
+	return i > 10
 }
 
-func main()  {
-  fmt.Println("Hello World")
-  hello()
-  fmt.Println(checkIf(11))
+func main() {
+	fmt.Println("Hello World")
+	hello()
+	fmt.Println(checkIf(11))
 
-  var num1 Integer = 123;
-  var ptr *Integer = &num1;
-  
-  fmt.Printf("%x=%d",ptr,*ptr)
+	var num1 Integer = 123
+	var ptr = &num1
+
+	fmt.Printf("%x=%d", ptr, *ptr)
 }
 
-func hello()  {
-  fmt.Println("Hello World 2")
+func hello() {
+	fmt.Println("Hello World 2")
 }
